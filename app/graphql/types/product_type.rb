0b54db5f384bc90f19963032c8d7c0829ecb2190
AzfaarQureshi <azfaar.qureshi@gmail.com>
2019-01-17 23:31:47 -0500

    Wrote GraphQL schema for ProductType, defining same fields as the model, and wrote allProducts query which returns all the products

---
+++ app/graphql/types/product_type.rb
@@ -0,0 +1,12 @@
+#Defining schema of type Product
+
+Types::ProductType = GraphQL::ObjectType.define do
+	name 'Product'
+
+	#Fields of Product
+	field :id, !types.ID
+	field :title, !types.String
+	field :price, !types.Float
+	field :inventory_count, !types.Int
+
+end
