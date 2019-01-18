7484c358f901130852710cffadbd31ef3c26e2b5
AzfaarQureshi <azfaar.qureshi@gmail.com>
2019-01-18 01:07:41 -0500

    adding purchaseProduct mutator and subsequent resolver.

---
+++ app/graphql/resolvers/purchase_product.rb
@@ -0,0 +1,12 @@
+class Resolvers::PurchaseProduct < GraphQL::Function
+	argument :id, !types.ID
+
+	type Types::ProductType
+	def call(_obj, args, _ctx)
+		prodToPurchase = Product.find(args[:id])
+		# if nil then return an error
+		prodToPurchase.inventory_count -= 1
+		prodToPurchase.save! # if not true then return an error
+		return prodToPurchase
+	end
+end
